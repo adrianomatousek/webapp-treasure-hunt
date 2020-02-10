741c370fac862cf086db94dbfdfeaf67efeb52f2
Doglum <danielgmorgan99@gmail.com>
2020-02-10 14:44:39 +0000

    PHP SQL example

--- www/html/index.php
+++ www/html/index.php
@@ -40,5 +40,21 @@
  ?>
 
 <?php
-echo 'hello world';
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "treasurehunt";
+$conn = new mysqli($servername,$username,$password,$database);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT name FROM student_users";
+$result = $conn->query($sql);
+// output data of each row
+while($row = $result->fetch_assoc()) {
+  echo $row['name'];
+}
+$conn->close();
 ?>
