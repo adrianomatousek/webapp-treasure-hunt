71ef72e4c31784ec5c396c0de837cdade1a72bfe
janczak <jmateusz26@gmail.com>
2020-02-11 19:27:50 +0000

    updated markers

--- www/html/script.js
+++ www/html/script.js
@@ -1,8 +1,9 @@
 var map;
 var markerList = [];
 var markers = markerList.length;
-var isDay = false;
 var windowOpen = false;
+var isDay = true;
+
 
 function myMap() {
 	map = new google.maps.Map(document.getElementById("googleMap"));
@@ -71,8 +72,7 @@ function removeMarker(id) {
 function closeAllMarkerWindows() {
 	for (i=0; i < markerList.length; i++) {
 		markerList[i].infowindow.close(map, marker);
-	}
-	
+	}	
 }
 
 function checkTime() {
@@ -92,42 +92,7 @@ function dayTime() {
 		center: new google.maps.LatLng(50.735882, -3.534206),
 		zoom: 16,
 		mapTypeId: google.maps.MapTypeId.ROADMAP,
-		styles: [
-			{
-				"featureType": "administrative",
-				"elementType": "geometry",
-				"stylers": [
-					{
-						"visibility": "off"
-					}
-				]
-			},
-			{
-				"featureType": "poi",
-				"stylers": [
-					{
-						"visibility": "off"
-					}
-				]
-			},
-			{
-				"featureType": "road",
-				"elementType": "labels.icon",
-				"stylers": [
-					{
-						"visibility": "off"
-					}
-				]
-			},
-			{
-				"featureType": "transit",
-				"stylers": [
-					{
-						"visibility": "off"
-					}
-				]
-			}
-		]
+		styles: map_theme_daytime
 	});
 }
 function nightTime() {
@@ -135,92 +100,6 @@ function nightTime() {
 		center: new google.maps.LatLng(50.735882, -3.534206),
 		zoom: 16,
 		mapTypeId: google.maps.MapTypeId.ROADMAP,
-		styles: [
-			{
-				"featureType": "poi",
-				"stylers": [
-					{ "visibility": "off" }
-				]
-			},
-
-			{ elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
-			{ elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
-			{ elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
-			{
-				featureType: 'administrative.locality',
-				elementType: 'labels.text.fill',
-				stylers: [{ color: '#d59563' }]
-			},
-			{
-				featureType: 'poi',
-				elementType: 'labels.text.fill',
-				stylers: [{ color: '#d59563' }]
-			},
-			{
-				featureType: 'poi.park',
-				elementType: 'geometry',
-				stylers: [{ color: '#263c3f' }]
-			},
-			{
-				featureType: 'poi.park',
-				elementType: 'labels.text.fill',
-				stylers: [{ color: '#6b9a76' }]
-			},
-			{
-				featureType: 'road',
-				elementType: 'geometry',
-				stylers: [{ color: '#38414e' }]
-			},
-			{
-				featureType: 'road',
-				elementType: 'geometry.stroke',
-				stylers: [{ color: '#212a37' }]
-			},
-			{
-				featureType: 'road',
-				elementType: 'labels.text.fill',
-				stylers: [{ color: '#9ca5b3' }]
-			},
-			{
-				featureType: 'road.highway',
-				elementType: 'geometry',
-				stylers: [{ color: '#746855' }]
-			},
-			{
-				featureType: 'road.highway',
-				elementType: 'geometry.stroke',
-				stylers: [{ color: '#1f2835' }]
-			},
-			{
-				featureType: 'road.highway',
-				elementType: 'labels.text.fill',
-				stylers: [{ color: '#f3d19c' }]
-			},
-			{
-				featureType: 'transit',
-				elementType: 'geometry',
-				stylers: [{ color: '#2f3948' }]
-			},
-			{
-				featureType: 'transit.station',
-				elementType: 'labels.text.fill',
-				stylers: [{ color: '#d59563' }]
-			},
-			{
-				featureType: 'water',
-				elementType: 'geometry',
-				stylers: [{ color: '#17263c' }]
-			},
-			{
-				featureType: 'water',
-				elementType: 'labels.text.fill',
-				stylers: [{ color: '#515c6d' }]
-			},
-			{
-				featureType: 'water',
-				elementType: 'labels.text.stroke',
-				stylers: [{ color: '#17263c' }]
-			}
-		]
+		styles: map_theme_nighttime
 	});
 }
