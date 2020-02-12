80cd1b4adc4193afe3e73f9a7c97fb58f1c524ff
janczak <jmateusz26@gmail.com>
2020-02-12 14:56:55 +0000

    updated markers and fixed animations

--- www/html/script.js
+++ www/html/script.js
@@ -2,64 +2,179 @@ var map;
 var markerList = [];
 var isDay = true;
 var markers = markerList.length;
-var windowOpen = false;
+var activeInfoWindow;
+var activeInfoLabel;
+var activeMarker;
+var showLabelOnMouseOver = true;  // displays small info/help label when mouse cursor is over a marker
+					
+								// could be one of the options that can be toggled on/off in settings
 
 function myMap() {
 	map = new google.maps.Map(document.getElementById("googleMap"));
-	dayTime();
-	addMarker(50.735882, -3.534206, 'Bob`s place');
+	/*
+	infoWindow = new google.maps.InfoWindow({
+		content: ''
+	});
+	*/
+	dayTime()
+	addMarker(50.735882, -3.534206, 'Bob`s place', 'A nice and cozy place. Very well known by all Exeter students.<br>Bob likes to spend his time here.</br>', true);
 	addMarker(50.734882, -3.535206);
 	addMarker(50.735882, -3.536206);
 	addMarker(50.736882, -3.534206);
+	//addMarker(50.736882, -3.534206, '2', true);
+	//addMarker(50.736882, -3.534206, '3', true);
+	//addMarker(50.736882, -3.534206, '4', true);
 	//removeMarker(3);
+	
+	bob();
+	
 }
 
-function addMarker(latValue, lngValue, name, draggable = false) {
+function addMarker(latValue, lngValue, name, description, /*imageURL,*/ draggable = false) {
 	markerNum = markers + 1;
 
 	var marker = new google.maps.Marker({
 		position: { lat: latValue, lng: lngValue },
 		map: map,
-		title: name,
+		//title: name,
 		label: {
 			color: 'white',
 			text: markerNum.toString(),
 			fontSize: '18px',
-			fontWeight: 'bold'
+			fontWeight: 'bold',
 		},
 		icon: {
 			url: 'img/chest.png',
-			scaledSize: new google.maps.Size(50, 50), // scaled size
-			origin: new google.maps.Point(0, 0) // origin
+			scaledSize: new google.maps.Size(50, 50),
+			origin: new google.maps.Point(0, 0)
 		},
 		draggable: draggable,
 		animation: google.maps.Animation.DROP
 	});
 	
-	if (!name){
+	if (!name || name.length < 3){
 		name = 'Treasure';
 	}
 	
-	var contentString = '<div id="content">' + '<div id="siteNotice">' + '</div>' +
-      '<h1 id="firstHeading" class="firstHeading">' + name + '</h1>' + '<div id="bodyContent">'+
-	  '<p>There is treasure waiting to be found here!</p>' + '</div>';
-
+	if (!description || description.length < 10){
+		var description = 'There is treasure to be found here!<br>Get here fast!</br>';
+	}
+	
+	/*
+	if (!imageURL || imageURL.length < 9){
+		imageURL = '';
+	}*/
+	
+	var contentString = '<div id="content"><div id="siteNotice">Treasure Location</div>' + 
+		'<h4 id="firstHeading" class="firstHeading">' + markerNum + '. ' + name + 
+		'</h4><div id="bodyContent"><p> ' + description + ' </p></div>' //+
+		//'<div style="background-image: url('+ imageURL +'); height: 200px; width: 300px;">Picture of Bob:</div>';
+	
+	/*
+	marker.addListener('click', function () {                
+        infoWindow.setContent(marker.contentString);
+        infoWindow.open(map, marker);
+    });
+	*/
+	
+	/* 
 	var infowindow = new google.maps.InfoWindow({
-		content: contentString
+		content: contentString,
+		isOpen: false
 	});
-
+	
 	marker.addListener('click', function() {
-		if (!windowOpen) {
+		//closeAllMarkerWindows();
+		if (anyWindowOpen) {
+			if (infowindow.isOpen){
+			infowindow.close(map, marker);
+			infowindow.isOpen = false;
+			}
+			else { 
+				//closeAllMarkerWindows();
+				infowindow.open(map, marker);
+				infowindow.isOpen = true;
+			}
+			anyWindowOpen = false;
+		}
+		else{  
 			infowindow.open(map, marker);
-			windowOpen = true;
+			infowindow.isOpen = true;
+			anyWindowOpen = true;
+		}	
+	});
+	*/
+	
+	var infoWindow = new google.maps.InfoWindow({
+		pixelOffset: new google.maps.Size(0,-16),
+		content: contentString,
+		//maxWidth: 300
+	});
+	
+	var infoLabel = new google.maps.InfoWindow({
+		pixelOffset: new google.maps.Size(0,-16),
+		content: '<div id="bodyContent"><p> This location contains a hidden treasure. Click for more info. </p></div>'
+	});
+	
+	marker.addListener('click', function () {
+		if (activeInfoLabel) {
+			activeInfoLabel.close(map, marker);
+		}
+		if (activeInfoWindow) {
+			activeInfoWindow.close();
+		}
+		if (activeInfoWindow !== infoWindow){
+			marker.setAnimation(google.maps.Animation.BOUNCE);
+			infoWindow.open(map, marker);
+			activeInfoWindow = infoWindow;
+			if (activeMarker) {
+				activeMarker.setAnimation(null);
+				activeMarker = null;
+			}
+			activeMarker = marker;
+			
 		}
 		else {
-			infowindow.close(map, marker);
-			//infowindow.close(map, marker);
-			windowOpen = false;
-		}
+			activeInfoWindow = null;
+			//activeMarker = null;
+		}	
 	});
-	markerList.push(marker)
+	
+	/*
+	var timer;
+	var timeout = function () {
+		alert('No movement!');
+	}
+	timer = setTimeout(timeout, 5000);
+	window.onmousemove = function() {
+		clearTimeout(timer);
+		timer = setTimeout(timeout, 5000);
+	};
+	*/
+	
+	if (showLabelOnMouseOver) {
+	
+		var mouseOnMarker = false;
+		
+		marker.addListener('mouseover', function() {
+			mouseOnMarker = true;
+			setTimeout(function(){
+				if(mouseOnMarker && !activeInfoWindow) {
+					infoLabel.open(map, marker); 
+					activeInfoLabel = infoLabel;
+				}
+			}, 2000);
+		});
+
+		marker.addListener('mouseout', function() {
+			mouseOnMarker = false;
+			setTimeout(function(){
+				infoLabel.close(map, marker); 
+			}, 1200);
+		});
+	}
+	
+	markerList.push(marker);
 	markers += 1;
 }
 
@@ -68,11 +183,20 @@ function removeMarker(id) {
 	marketList.splice(id);
 }
 
-function closeAllMarkerWindows() {
+function removeAllMarkers() {
 	for (i=0; i < markerList.length; i++) {
+		marketList[id].setMap(null);
+	}
+	marketList = null;
+}
+
+/*
+function closeAllMarkerWindows() {
+	for (i=0; i < markerList.length; i++){
 		markerList[i].infowindow.close(map, marker);
 	}	
 }
+*/
 
 function checkTime() {
 	if (isDay) {
@@ -87,7 +211,6 @@ function checkTime() {
 
 function dayTime() {
 	map.setOptions({
-		// center:new google.maps.LatLng(51.508742,-0.120850),
 		center: new google.maps.LatLng(50.735882, -3.534206),
 		zoom: 16,
 		mapTypeId: google.maps.MapTypeId.ROADMAP,
@@ -101,4 +224,39 @@ function nightTime() {
 		mapTypeId: google.maps.MapTypeId.ROADMAP,
 		styles: map_theme_nighttime
 	});
+}
+
+
+
+
+
+
+
+function bob(size = 200) {
+	var marker = new google.maps.Marker({
+		position: { lat: 50.745882, lng: -3.534206 },
+		map: map,
+		//title: '',
+		label: {
+			color: 'black',
+			text: 'Bob',
+			fontSize: '18px',
+			fontWeight: 'bold'
+		},
+		icon: {
+			url: 'img/creeper.png',
+			scaledSize: new google.maps.Size(size, size),
+			origin: new google.maps.Point(0, 0)
+		},
+		draggable: true,
+		animation: google.maps.Animation.BOUNCE
+	});
+	var infoWindow = new google.maps.InfoWindow({
+		pixelOffset: new google.maps.Size(0,-15),
+		content: '<div id="content"><div id="siteNotice"></div><h4 id="firstHeading" class="firstHeading">Bob</h4><div id="bodyContent"><p>Beware! A wild Bob has appeared!</p></div>'
+		//maxWidth: 50
+	});
+	marker.addListener('click', function () {                
+        infoWindow.open(map, marker);
+    });
 }
