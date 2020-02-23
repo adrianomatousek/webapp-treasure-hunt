61f46d27ab5d5ccdd229532d2bbec19883da1bd3
Atmaram Chodancar <atmaramch@gmail.com>
2020-02-23 13:41:55 +0000

    style login page

--- www/html/index.php
+++ www/html/index.php
@@ -4,21 +4,43 @@
   <title>Login</title>
   <link rel="stylesheet" type="text/css" href="login_styles.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
   </head>
 
   <body>
 
-    <div class = "container">
-      <a href="#!" class="test"><i class="material-icons md-48">account_circle</i></a>
-      <!-- Login form -->
-        <h1>Login</h1>
-        <form class = "log" name = "login"method="post" onsubmit="return validation()" action = "index.php">
-  	      <input type ="text" name ="inputUsername" placeholder="Username"><br>
-          <input type = "password" name = "inputPassword" placeholder="Password">
-          <input type="submit" name = "login" value="Login">
-          <a href="#">Forgotten Password</a><br>
+    <div id="container">
+    <div class="col s12 z-depth-6 card-panel">
+      <form class="login-form" name = "login" method="POST" action = "index.php">
+        <div class="row">
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <i class="material-icons prefix">account_circle</i>
+            <input id="inputUsername" type="text">
+            <label for="inputUsername">Username</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <i class="material-icons prefix">lock_outline</i>
+            <input id="inputPassword" type="password">
+            <label for="inputPassword">Password</label>
+          </div>
+        </div>
 
-        </form>
+        <div class="row">
+           <div class="input-field col s12">
+             <input type="submit" class="btn waves-effect waves-light col s12" name="login" value="Login" />
+           </div>
+         </div>
+        <div class="row">
+          <div class="input-field col s6 m6 l6">
+              <p class="margin right-align medium-small"><a href="#">Forgot password?</a></p>
+          </div>
+        </div>
+
+      </form>
     </div>
 
     <script>
@@ -34,6 +56,7 @@
 
     </script>
   </body>
+
 
 </html>
 
@@ -53,9 +76,11 @@ $result = $conn->query($sql);
 $found = False;
 // output data of each row
 if (isset($_POST['login']) && !empty($_POST['inputUsername']) && !empty($_POST['inputPassword'])) {
+  header("Location: test.html");
    while($row = $result->fetch_assoc()) {
      if ($_POST['inputUsername'] == $row['username'] &&
      hash('sha256',$_POST['inputPassword']) == $row['hashPass']) {
+        header("Location: test.html");
         echo 'Correct password for ',$row['username'];
         $found = True;
      }
