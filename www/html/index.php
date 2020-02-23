51d8c4729df895c6031e5d862d98bacc0f80bb2c
Atmaram Chodancar <atmaramch@gmail.com>
2020-02-23 16:45:07 +0000

    style login page

--- www/html/index.php
+++ www/html/index.php
@@ -10,37 +10,38 @@
   <body>
 
     <div id="container">
-    <div class="col s12 z-depth-6 card-panel">
-      <form class="login-form" name = "login" method="POST" action = "index.php">
-        <div class="row">
-        </div>
-        <div class="row">
-          <div class="input-field col s12">
-            <i class="material-icons prefix">account_circle</i>
-            <input id="inputUsername" type="text">
-            <label for="inputUsername">Username</label>
+      <div class="col s12 z-depth-6 card-panel">
+        <form form class = "log" name = "login" method="post" onsubmit="return validation()" action = "index.php">
+          <div class="row">
           </div>
-        </div>
-        <div class="row">
-          <div class="input-field col s12">
-            <i class="material-icons prefix">lock_outline</i>
-            <input id="inputPassword" type="password">
-            <label for="inputPassword">Password</label>
+          <div class="row">
+            <div class="input-field col s12">
+              <i class="material-icons prefix">account_circle</i>
+              <input class="validate" id="inputUsername" type="text">
+              <label for="inputUsername">Username</label>
+            </div>
           </div>
-        </div>
+          <div class="row">
+            <div class="input-field col s12">
+              <i class="material-icons prefix">lock_outline</i>
+              <input id="inputPassword" type="password">
+              <label for="inputPassword">Password</label>
+            </div>
+          </div>
 
-        <div class="row">
-           <div class="input-field col s12">
-             <input type="submit" class="btn waves-effect waves-light col s12" name="login" value="Login" />
-           </div>
-         </div>
-        <div class="row">
-          <div class="input-field col s6 m6 l6">
-              <p class="margin right-align medium-small"><a href="#">Forgot password?</a></p>
+          <div class="row">
+            <div class="input-field col s12">
+              <input type="submit" class="btn waves-effect waves-light col s12" id="btnsubmit" name="submit" value="Login" />
+            </div>
           </div>
-        </div>
+          <div class="row">
+            <div class="input-field col s6 m6 l6">
+                <p class="margin right-align medium-small"><a href="#">Forgot password?</a></p>
+            </div>
+          </div>
 
-      </form>
+        </form>
+      </div>
     </div>
 
     <script>
@@ -66,6 +67,7 @@ $username = "root";
 $password = "";
 $database = "treasurehunt";
 $conn = new mysqli($servername,$username,$password,$database);
+
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
@@ -76,11 +78,9 @@ $result = $conn->query($sql);
 $found = False;
 // output data of each row
 if (isset($_POST['login']) && !empty($_POST['inputUsername']) && !empty($_POST['inputPassword'])) {
-  header("Location: test.html");
    while($row = $result->fetch_assoc()) {
      if ($_POST['inputUsername'] == $row['username'] &&
      hash('sha256',$_POST['inputPassword']) == $row['hashPass']) {
-        header("Location: test.html");
         echo 'Correct password for ',$row['username'];
         $found = True;
      }
