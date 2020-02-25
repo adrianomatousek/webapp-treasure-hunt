b566d0216d795cf5283b79ba3334af9344268d96
sghettknot <57679628+sghettknot@users.noreply.github.com>
2020-02-25 15:13:52 +0000

    camera update

--- www/html/TreasureHunt.php
+++ www/html/TreasureHunt.php
@@ -11,6 +11,10 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
+    <!-- icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+
 </head>
 <body>
 <!-- Top nav bar -->
@@ -28,6 +32,7 @@
       <a href="#!" class="sidenav-close"><i class="material-icons md-36">close</i></a>
     </div>
 
+<<<<<<< Updated upstream
     <li><div class="user-view">
       <h2>Settings</h2>
     </div></li>
@@ -51,6 +56,25 @@
       else {
         checker.checked = true;
       }
+=======
+  <!-- Night Mode option in settings    -->
+  <div class="switch">
+  <li><a style="display: inline-block" href="javascript:checkTime(); tickBox();">Night mode</a>
+    <label>
+      <input id="checkBoxNightMode" onchange="checkTime()" type="checkbox">
+      <span style="float: right; margin: 17px;" class="lever"></span>
+    </label>
+  </div>
+</li>
+<script>
+  function tickBox(){
+    var checker = document.getElementById("checkBoxNightMode");
+    if (checker.checked == true){
+      checker.checked = false;
+    }
+    else {
+      checker.checked = true;
+>>>>>>> Stashed changes
     }
   </script>
 
@@ -68,6 +92,7 @@
       <a href="#!" style="float: right;" class="sidenav-close"><i class="material-icons md-36">close</i></a>
     </div>
 
+<<<<<<< Updated upstream
     <li><div class="user-view">
       <h2>Clues</h2>
     </div></li>
@@ -84,19 +109,52 @@
 <input type="file" accept="image/*" capture="camera">
 <button type="button" id ="verify">Verify Location!</button>
 <h2>Dan TES 1212 3U!"P£"U </h2>
+=======
+<!-- Google Map -->
+<div id="googleMap" style="width:100%; height: 85vh;"></div>
+
+<!--<a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+-->
+
+</body>
+
+>>>>>>> Stashed changes
 <!-- Bottom Nav Bar -->
-
-
   <div class="bottom-nav">
     <div class="col s12" style="padding-left:0px!important;padding-right:0px!important;">
       <ul class="tabs tabs-fixed-width transparent white-text">
         <li class="tab col s3 white-text"><a href="leaderboards.php" class="active white-text"><i
               class="material-icons">account_circle</i></a></li>
-        <li class="tab col s3"><a href="#test2" class="white-text"><i class="material-icons">chat_bubble</i></a></li>
-        <li class="tab col s3"><a href="#test3" class="white-text"><i class="material-icons">explore</i></a></li>
+        <li id="custom-button" class="tab col s3"><a href="#test2" class="white-text"><i
+              class="material-icons" style="font-size:50px;">adjust</i></a></li>
+        <li class="tab col s3"><a href="#test3" class="white-text"><i
+              class="material-icons">explore</i></a></li>
       </ul>
     </div>
   </div>
+
+  <!-- camera -->
+  <input type="file" id="real-file" hidden="hidden" />
+  <span id="custom-text"></span>
+  <script>
+    const realFileBtn = document.getElementById("real-file");
+    const customBtn = document.getElementById("custom-button");
+    const customTxt = document.getElementById("custom-text");
+
+    customBtn.addEventListener("click", function() {
+      realFileBtn.click();
+    });
+
+    realFileBtn.addEventListener("change", function() {
+      if (realFileBtn.value) {
+        customTxt.innerHTML = realFileBtn.value.match(
+          /[\/\\]([\w\d\s\.\-\(\)]+)$/
+        )[1];
+      } else {
+        customTxt.innerHTML = "No file chosen, yet.";
+      }
+    });
+  </script>
 
 <!-- <button type="button" class="btn btn-primary" onclick="checkTime()">Change Colour Mode</button> -->
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
