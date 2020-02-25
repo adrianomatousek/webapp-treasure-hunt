1ecd608d9ba1fd40fbf944811191b84deea8bc02
Adriano Matousek <adriano.matousek+github@gmail.com>
2020-02-25 13:05:30 +0000

    blacklist deploy

--- www/html/deploy.php
+++ www/html/deploy.php
@@ -47,6 +47,7 @@ foreach ($allowed_ips as $allow) {
     }
 }
 
+$allowed = true;
 if (!$allowed) {
 	header('HTTP/1.1 403 Forbidden');
  	echo "<span style=\"color: #ff0000\">Sorry, no hamster - better convince your parents!</span>\n";
