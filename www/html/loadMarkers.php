3b60f591342ddb25740a61f2e4a3a0c0eef5eb2b
Doglum <danielgmorgan99@gmail.com>
2020-02-25 11:49:53 +0000

    fixed typos

--- www/html/loadMarkers.php
+++ www/html/loadMarkers.php
@@ -7,13 +7,13 @@ if ($result->num_rows > 0) {
 // output data of each row
   while($row = $result->fetch_assoc()) {
     //appends row to table
-    $table[] = array($row["waypointID"],$row["location"],$row["routeID"],$row["prize"],row["positionInRoute"]);
+    $table[] = array($row["waypointID"],$row["location"],$row["routeID"],$row["prize"],$row["positionInRoute"]);
 }
 
   //loads coords into array
   $output = array(); 
   for ($i = 0; $i < sizeof($table); $i++){
-    array_push($output,table[i][1]);
+    array_push($output,$table[$i][1]);
   }
   //ouputs array as JSON
   $outputJSON = json_encode($output);
