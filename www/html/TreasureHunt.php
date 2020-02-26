fe49b3a1644f32ecbc83ef51022f3479be94ca0d
Atmaram Chodancar <atmaramch@gmail.com>
2020-02-26 14:04:16 +0000

    bottom nav fix?

--- www/html/TreasureHunt.php
+++ www/html/TreasureHunt.php
@@ -124,7 +124,7 @@
         <li class="tab col s3 white-text"><a href="javascript: bottomNavGoTo(0);" class="active black-text"><i
               class="material-icons">account_circle</i></a></li>
         <li class="tab col s3"><a href="javascript: bottomNavGoTo(1);" class="black-text"><i class="material-icons"
-              style="font-size:55px; position: relative; top: -3px;">adjust</i></a></li>
+              style="font-size:50px;">adjust</i></a></li>
         <li class="tab col s3"><a href="javascript: bottomNavGoTo(2);" class="black-text"><i
               class="material-icons">explore</i></a>
         </li>
