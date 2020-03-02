fe6641d944fd5c20d8aa2652e05935243d2fb407
Atmaram Chodancar <atmaramch@gmail.com>
2020-03-02 11:25:28 +0000

    geo

--- www/html/map_script.js
+++ www/html/map_script.js
@@ -63,11 +63,30 @@ function myMap() {
 
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(function (position) {
+				var pos = {
+					lat: position.coords.latitude,
+					lng: position.coords.longitude
+				};
+				console.log(pos);
+				infoWindow.setPosition(pos);
+				infoWindow.setContent('Location found.');
+				infoWindow.open(map);
+				map.setCenter(pos);
+			},
+			function () {
+				handleLocationError(true, infoWindow, map.getCenter());
+			});
+	} else {
+		// Browser doesn't support Geolocation
+		handleLocationError(false, infoWindow, map.getCenter());
+	}
 
-				console.log(position.coords.latitude);
-
-			}
-
+	function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+		infoWindow.setPosition(pos);
+		infoWindow.setContent(browserHasGeolocation ?
+			'Error: The Geolocation service failed.' :
+			'Error: Your browser doesn\'t support geolocation.');
+		infoWindow.open(map);
 	}
 
 
