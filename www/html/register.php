c96a586edf7bc8d8505586ad8bb017432640cffc
tentenmen100 <little.donut@hotmail.co.uk>
2020-03-05 21:55:12 +0000

    Registering input is now sanitized fix 5

--- www/html/register.php
+++ www/html/register.php
@@ -114,7 +114,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
       header("location: index.php");
     // }
   }
-  else if ($_POST['register']){
+  else if ($SERVER{'REQUEST_METHOD'] == "POST"}){
     echo '<script type="text/javascript"> alert("Please enter a username and password."); </script>';
   }
 }
