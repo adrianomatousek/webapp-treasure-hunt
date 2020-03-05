6a7692dfb02fab547c027635fcea99909b9f7848
Atmaram Chodancar <atmaramch@gmail.com>
2020-03-05 18:00:53 +0000

    register

--- www/html/register.php
+++ www/html/register.php
@@ -77,7 +77,7 @@
  if (isset($_POST['register']) && !empty($_POST['inputUsername']) && !empty($_POST['inputPassword'])) {  //login validation
   $user = $_POST['inputUsername'];
   $salt = random_bytes(16);
-  $pwd = hash('sha256',$_POST['inputPassword']);
+  $pwd = hash('sha256',$_POST['inputPassword'].$salt);
   $sql = "INSERT INTO student_users (username,hashPass,salt,accessLevel,score,name,email,gamekeeperID) VALUES ('$user', '$pwd', '$salt','acessLevel',0,'name','email','gamekeeperID')";
   if ($result = $conn->query($sql)) {
   echo "added";
