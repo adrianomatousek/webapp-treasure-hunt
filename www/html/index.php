e8cca966710ab7cc21cbe96c785831a36ce27d9b
tentenmen100 <little.donut@hotmail.co.uk>
2020-03-05 19:33:05 +0000

    More efficient login checker 2

--- www/html/index.php
+++ www/html/index.php
@@ -75,30 +75,74 @@
 
 </html>
 <?php
-require ("connection.php");
-session_start();
-$sql = "SELECT username,hashPass,salt FROM student_users";
-$result = $conn->query($sql);
-$found = False;
+// require ("connection.php");
+// session_start();
+// $sql = "SELECT username,hashPass,salt FROM student_users";
+// $result = $conn->query($sql);
+// $found = False;
 
-$row_test = "";
-// output data of each row
-if (isset($_POST['login']) && !empty($_POST['inputUsername']) && !empty($_POST['inputPassword'])) {  //login validation
-   while($row = $result->fetch_assoc()) {
-     if ($_POST['inputUsername'] == $row['username']) {
-      $attempt_hash = hash('sha256',$_POST['inputPassword'].$row['salt']);  
-       if ($attempt_hash == $row['hashPass']) {
-        echo 'Correct password for ',$row['username'];
-        header('Location: TreasureHunt.php');
-        $found = True;
-       }
-     }
-   }
-   if(!$found){
-     echo "Incorrect Details";
-     echo "<br>Name: " . $_POST['inputUsername'];
-     echo "<br>Pass: " . $_POST['inputPassword'];
-     echo "<br>Hash: " . hash('sha256',$_POST['inputPassword'].'??Z+79?`?w85|');
-   }
+// $row_test = "";
+// // output data of each row
+// if (isset($_POST['login']) && !empty($_POST['inputUsername']) && !empty($_POST['inputPassword'])) {  //login validation
+//    while($row = $result->fetch_assoc()) {
+//      if ($_POST['inputUsername'] == $row['username']) {
+//       $attempt_hash = hash('sha256',$_POST['inputPassword'].$row['salt']);  
+//        if ($attempt_hash == $row['hashPass']) {
+//         echo 'Correct password for ',$row['username'];
+//         header('Location: TreasureHunt.php');
+//         $found = True;
+//        }
+//      }
+//    }
+//    if(!$found){
+//      echo "Incorrect Details";
+//      echo "<br>Name: " . $_POST['inputUsername'];
+//      echo "<br>Pass: " . $_POST['inputPassword'];
+//      echo "<br>Hash: " . hash('sha256',$_POST['inputPassword'].'??Z+79?`?w85|');
+//    }
+// }
+
+
+session_start();
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+  header("Location: TreasureHunt.php");
+  exit;
+}
+
+require_once "connection.php";
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+   $CheckUsername = $_POST['inputUsername'];
+   $CheckPassword = $_POST['inputPassword'];
+
+
+  //  $sqlQuery = "SELECT * FROM student_users";
+  //  $database = $conn->query($sqlQuery);
+$query= mysql_query("SELECT username, hashPass, accessLevel FROM student_users WHERE username = ".$_SESSION['$CheckUsername']." ");
+$database = mysql_fetch_array($query);
+
+   while ($user = $database->fetch_assoc()){
+    if (($user['username'] == $CheckUsername) && (hash('sha256',$CheckPassword.$user['salt']) == $user['hashPass'])) {
+      $_SESSION["loggedin"] = true;
+      $_SESSION["id"] = $user["id"];
+      $_SESSION["username"] = $CheckUsername;
+      $_SESSION["privileges"] = $user["accessLevel"];
+
+    header("Location: StockPage.php");
+    die();
+  }
+}
+$error = "Incorrect username or password.";
+
+if(isset($_SESSION["counter"])) {
+  $_SESSION["counter"] = $_SESSION["counter"] + 1;
+  if (($_SESSION["counter"]) > 3){
+    $error = "Too many incorrect login attempts. Waited 10 seconds. Please try again.";
+  }
+}
+else{
+  $_SESSION["counter"] = 0;
+}
 }
 ?>
