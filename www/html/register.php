d705f70baefa94b958b5ca28d63ef9c690a71eb2
tentenmen100 <little.donut@hotmail.co.uk>
2020-03-05 23:15:28 +0000

    Registering input is now sanitized fix 22

--- www/html/register.php
+++ www/html/register.php
@@ -111,8 +111,8 @@ require ("connection.php");
       $salt = generateRandomString(16);
       $pwd = hash('sha256',$_POST['inputPassword'].$salt);
       // $sql = "INSERT INTO student_users (username,hashPass,salt,accessLevel,score,name,email,gamekeeperID) VALUES ('$user', '$pwd', '$salt','Student',0,'name','email','ChiefGamekeeper')";
-      $addAcc = $conn->prepare("INSERT INTO `student_users` (username, hashPass, salt, accessLevel, score,name, email, gamekeeperID) VALUES (?,?,?,?,?,?,?,?)");
-      $addAcc->bind_param('ssssisss', $user, $pwd, $salt, 'Student',0, 'fullName','email','chiefGamekeeper');
+      $addAcc = $conn->prepare("INSERT INTO `student_users` (username, hashPass, salt, accessLevel, name, email, gamekeeperID) VALUES (?,?,?,?,?,?,?)");
+      $addAcc->bind_param('sssssss', $user, $pwd, $salt, 'Student', 'fullName','email','chiefGamekeeper');
       $addAcc->execute();
       $addAcc->close();
 
