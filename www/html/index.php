f17e9871e3341a574ef52555a612b26e3ca8ba98
Adriano Matousek <adriano.matousek+github@gmail.com>
2020-03-05 18:46:34 +0000

    log

--- www/html/index.php
+++ www/html/index.php
@@ -112,6 +112,7 @@ if (isset($_POST['login']) && !empty($_POST['inputUsername']) && !empty($_POST['
      echo "<br>Hash: " . hash('sha256',$_POST['inputPassword'].$row['salt']);
      $attempt_hash = hash('sha256',$_POST['inputPassword'].$row['salt']);
      if ($attempt_hash == $row['hashPass']) {
+       echo "$attempt_hash <br>";
       echo 'Correct password for ',$row['username'];
       header('Location: TreasureHunt.php');
       $found = True;
