56dd2268fcfc25f8f153a135f0acf60e5e78ff6a
Adriano Matousek <adriano.matousek+github@gmail.com>
2020-03-05 18:47:29 +0000

    log

--- www/html/index.php
+++ www/html/index.php
@@ -111,7 +111,7 @@ if (isset($_POST['login']) && !empty($_POST['inputUsername']) && !empty($_POST['
      echo "<br>Pass: " . $_POST['inputPassword'];
      echo "<br>Hash: " . hash('sha256',$_POST['inputPassword'].$row['salt']);
      $attempt_hash = hash('sha256',$_POST['inputPassword'].$row['salt']);
-     echo "$attempt_hash <br>";
+     echo "<br>$attempt_hash";
      if ($attempt_hash == $row['hashPass']) {
        echo "$attempt_hash <br>";
       echo 'Correct password for ',$row['username'];
