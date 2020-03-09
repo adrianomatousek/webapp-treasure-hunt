c1dabeb334e4ea7d0841a63ff259a9a3916000ef
Doglum <danielgmorgan99@gmail.com>
2020-03-09 14:31:11 +0000

    typo

--- www/html/saveRoute.php
+++ www/html/saveRoute.php
@@ -3,7 +3,7 @@ require("connection.php");
 $lastRoute = $conn->query("SELECT * FROM routes ORDER by routeID DESC LIMIT 1");
 if ($lastRoute->num_rows > 0) {
     // output data of each row
-    while($row = $result->fetch_assoc()) {
+    while($row = $lastRoute->fetch_assoc()) {
         echo $row["routeID"];
     }
 }
