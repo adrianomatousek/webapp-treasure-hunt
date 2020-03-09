a4e0062ad519ac8e944d0ee41888ed8982e5e5a2
Adriano Matousek <adriano.matousek+github@gmail.com>
2020-03-09 15:21:44 +0000

    loop board

--- www/html/score_script.js
+++ www/html/score_script.js
@@ -10,19 +10,27 @@ function addScore(amount) {
 
 
 //AJAX calls to retrive data from data base from query.php
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-        var gameData = JSON.parse(this.response);
-        var alldata = ""; //all data from database stored in variable
-        var length = gameData.length;
 
-        for (x = 0; x < length; x++) { //table data and drop down list data retrieved
-            alldata += "<tr>" + "<td>" + gameData[x].username + "</td><td>" +
-                gameData[x].score + "</td><td>" + gameData[x].score + "</td><td>" + gameData[x].username + "</td></tr>";
+function update_board() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("mytable").innerHTML = "";
+            var gameData = JSON.parse(this.response);
+            console.log(gameData);
+            var alldata = ""; //all data from database stored in variable
+            var length = gameData.length;
+
+            for (x = 0; x < length; x++) { //table data and drop down list data retrieved
+                alldata += "<tr>" + "<td>" + gameData[x].username + "</td><td>" +
+                    gameData[x].score + "</td><td>" + gameData[x].score + "</td><td>" + gameData[x].username + "</td></tr>";
+            }
+            document.getElementById("mytable").innerHTML = alldata;
         }
-        document.getElementById("mytable").innerHTML = alldata;
     }
+    xmlhttp.open("GET", "leaderboardsData.php", true);
+    xmlhttp.send();
 }
-xmlhttp.open("GET", "leaderboardsData.php", true);
-xmlhttp.send();
+
+update_board()
+setTimeout(update_board, 3000);
