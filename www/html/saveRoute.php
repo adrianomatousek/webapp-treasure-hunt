4f372d258a6eab871fed8022aedb5dcefde513db
Doglum <danielgmorgan99@gmail.com>
2020-03-09 14:35:49 +0000

    get new route ID

--- www/html/saveRoute.php
+++ www/html/saveRoute.php
@@ -1,10 +1,14 @@
 <?php
 require("connection.php");
+
+//grabs the routeID of the last entry in the database, puts in newRouteID
+$newRouteID;
 $lastRoute = $conn->query("SELECT * FROM routes ORDER by routeID DESC LIMIT 1");
-if ($lastRoute->num_rows > 0) {
+if ($lastRoute->num_rows == 1) {
     // output data of each row
     while($row = $lastRoute->fetch_assoc()) {
-        echo $row["routeID"];
+        $newRouteID = $row["routeID"] + 1;
     }
 }
+echo $newRouteID;
 ?>
