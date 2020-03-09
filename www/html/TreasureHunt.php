84184fdb39c37b2fe3ac0dc35838ee0be3655a6a
tentenmen100 <little.donut@hotmail.co.uk>
2020-03-09 17:47:30 +0000

    TreasureHunt.php kicks unauthorised users back to login 2

--- www/html/TreasureHunt.php
+++ www/html/TreasureHunt.php
@@ -208,8 +208,9 @@
 <?php
 session_start();
 
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] != true){
-  header("Location: index.php");
+if (!(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)){
+  header("Location: TreasureHunt.php");
+  exit;
 }
 
 ?>
