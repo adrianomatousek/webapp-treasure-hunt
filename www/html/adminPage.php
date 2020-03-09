fec6de5365c17d1c8257b81dc4c82e4a50093ad3
tentenmen100 <little.donut@hotmail.co.uk>
2020-03-09 16:07:46 +0000

    Testing admin exists fix 5

--- www/html/adminPage.php
+++ www/html/adminPage.php
@@ -1,25 +1,27 @@
 <!DOCTYPE html>
 <html>
-<head>
-</head>
-<body>
-<form>
-    <p>Select privileges to give user:</p>
-    <select name="privileges">
+  <head>
+    <h1>
+    </h1>
+  </head>
+  <body>
+    <form>
+      <p>Select privileges to give user:</p>
+      <select name="privileges">
         <option value=""></option>
         <option value="Student">Student</option>
         <option value="Gamekeeper">Gamekeeper</option>
         <option value="Admin">Admin</option>
-    </select><br>
+      </select><br>
 
-    <p>Select user to give privileges:</p>
-    <input class="validate" id="inputUsername" name="inputUsername" maxlength="10" type="text" required/>
+      <p>Select user to give privileges:</p>
+      <input class="validate" id="inputUsername" name="inputUsername" maxlength="10" type="text" required/>
                 
-    <p>Assign privileges to user:</p>
-    <button type='submit' name='assign'>Assign privileges</button>
+      <p>Assign privileges to user:</p>
+      <button type='submit' name='assign'>Assign privileges</button>
             
-</form>
-</body>
+    </form>
+  </body>
 </html>
 
 <?php
@@ -39,7 +41,7 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
 }
 
 // if (isset($_POST['assign']) && !empty($_POST['inputUsername'])) {
-  if($_SERVER["REQUEST_METHOD"] == "POST")
+  if(isset($_POST['assign'])){
     $user = $_POST['inputUsername'];
 
     $query = $conn->prepare("SELECT COUNT(*) as usernameNo FROM `student_users` WHERE username = ?");
@@ -55,12 +57,12 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     $query->close();
 
     if ($usernameCount !== 1){
-      echo '<script type="text/javascript"> alert("Invalid username""); </script>';
+      echo '<script type="text/javascript"> alert("Invalid username"); </script>';
     }
     if ($username == 1){
       $newAccessLevel = $_POST['privileges'];
       
-      $statement = 'UPDATE student_users SET accessLevel=? WHERE username=?';
+      $statement = "UPDATE `student_users` SET accessLevel=? WHERE username=?";
       
       $setLevel = $conn->prepare($statement);
       $setLevel->bind_param('ss', $newAccessLevel, $user);
