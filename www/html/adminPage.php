122b1b3c2ac70c9fcc0c2f8e28cc23cd9effd88e
tentenmen100 <little.donut@hotmail.co.uk>
2020-03-09 14:05:02 +0000

    Addition of adminPage to assign privileges

---
+++ www/html/adminPage.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<form>
+    <p>Select privileges to give user:</p>
+    <select name="privileges">
+        <option value="Student">Student</option>
+        <option value="Gamekeeper">Gamekeeper</option>
+        <option value="Admin">Admin</option>
+    </select><br>
+
+    <p>Select user to give privileges:</p>
+    <input class="validate" id="inputUsername" name="inputUsername" maxlength="10" type="text" required/>
+                
+    <p>Assign privileges to user:</p>
+    <button type='submit' name='assign'>Assign privileges</button>
+            
+</form>
+</html>
+
+<?php>
+
+require_once ("connection.php");
+
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+  header("Location: TreasureHunt.php");
+  exit;
+}
+
+if (isset($_POST['assign']) && !empty($_POST['inputUsername'])) {
+    $user = $_POST['inputUsername'];
+
+    $query = $conn->prepare("SELECT COUNT(*) as usernameNo FROM `student_users` WHERE username = ?");
+    //Fills prepared statement with a string, avoids injection and allows us to check DB.
+    $query->bind_param("s", $user);
+    //Executes query and stores it in memory.
+    $query->execute();
+    
+    $result = $query->get_result();
+    $data = $result->fetch_assoc();
+    $usernameCount = $data['usernameNo'];
+
+    $query->close();
+
+    if ($usernameCount !== 1){
+      echo '<script type="text/javascript"> alert("Invalid username""); </script>';
+    }
+    $newAccessLevel = $_POST['privileges'];
+    
+    $statement = 'UPDATE student_users SET accessLevel=? WHERE username=?';
+    
+    $selectLevel = $conn->prepare($statement);
+    $setLevel->bind_param('ss', $newAccessLevel, $user);
+    $setLevel->execute();
+    $setLevel->close();
+}
+<?>
