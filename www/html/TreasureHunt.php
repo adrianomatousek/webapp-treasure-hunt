30227a11afe8745a30204b88f3ae581617a73db2
Adriano Matousek <adriano.matousek+github@gmail.com>
2020-03-09 13:50:32 +0000

    leaderboard to score script

--- www/html/TreasureHunt.php
+++ www/html/TreasureHunt.php
@@ -197,26 +197,6 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
   <script src="jsQR.js"></script>
   <script src="camera.js"></script>
-
-  <script>
-    //AJAX calls to retrive data from data base from query.php
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        var gameData = JSON.parse(this.response);
-        var alldata = ""; //all data from database stored in variable
-        var length = gameData.length;
-
-        for (x = 0; x < length; x++) { //table data and drop down list data retrieved
-          alldata += "<tr>" + "<td>" + gameData[x].username + "</td><td>" +
-            gameData[x].score + "</td><td>" + gameData[x].score + "</td><td>" + gameData[x].username + "</td></tr>";
-        }
-        document.getElementById("mytable").innerHTML = alldata;
-      }
-    }
-    xmlhttp.open("GET", "leaderboardsData.php", true);
-    xmlhttp.send();
-  </script>
 </body>
 
 </html>
