48cc8825fb01be1153d31674be5f86b6aa98429d
Doglum <danielgmorgan99@gmail.com>
2020-03-09 14:29:45 +0000

    added saveRoute php

---
+++ www/html/saveRoute.php
@@ -0,0 +1,10 @@
+<?php
+require("connection.php");
+$lastRoute = $conn->query("SELECT * FROM routes ORDER by routeID DESC LIMIT 1");
+if ($lastRoute->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo $row["routeID"];
+    }
+}
+?>
