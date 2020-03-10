050c4bd859ffad2b9ad82601b0ac064d8c79f725
Doglum <danielgmorgan99@gmail.com>
2020-03-10 14:19:19 +0000

    revert

--- www/html/gamekeeper_map.js
+++ www/html/gamekeeper_map.js
@@ -39,7 +39,6 @@ function saveRoute(){
 		type:"POST",
 		dataType:"json",
 		url:"saveRoute.php",
-		contentType: 'application/json; charset=utf-8',
 		data: {passedData: postData},
 		.done: function(data){
 			alert("Route added");
