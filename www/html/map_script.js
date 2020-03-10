6fa58b03284ccd15086813f40fbd56c97be6bf34
janczak <jmateusz26@gmail.com>
2020-03-10 05:40:32 +0000

    implementing settings & scaling size of markers

--- www/html/map_script.js
+++ www/html/map_script.js
@@ -573,7 +573,7 @@ function dayTime() {
 			markerList[i].setLabel(label);
 			var icon = markerList[i].getIcon();
 			icon.scaledSize = new google.maps.Size(20, 20);
-			icon.labelOrigin = new google.maps.Point(10, 24);
+			icon.labelOrigin = new google.maps.Point(10, 28);
 		}
 	}
 	if (customMarker) {
