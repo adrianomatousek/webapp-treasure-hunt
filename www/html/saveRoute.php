2ed138e7a94da079e5cc2bd2fa8a98fb3ad6859b
tentenmen100 <little.donut@hotmail.co.uk>
2020-03-10 23:57:13 +0000

    saveRoute, newRouteID is int not string

--- www/html/saveRoute.php
+++ www/html/saveRoute.php
@@ -38,7 +38,7 @@ if ($lastWaypoint->num_rows == 1) {
 $addRoute = $conn->prepare("INSERT INTO `routes` VALUES (?,?,?)");
 //Ideally passed as parameters (don't think you can pass as strings in bind_param).
 //Parameters need to be replaced with actual values that we can use and send.
-$addRoute->bind_param('sss', $newRouteID, $routeName, $_SESSION['keeperID']);
+$addRoute->bind_param('iss', $newRouteID, $routeName, $_SESSION['keeperID']);
 $addRoute->execute();
 $addRoute->close();
 
