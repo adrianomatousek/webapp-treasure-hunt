0624efa0c5421a673bb2986e91fcfbf28c670fbf
tentenmen100 <little.donut@hotmail.co.uk>
2020-03-11 19:22:02 +0000

    typeError for markerList fixed

--- www/html/map_script.js
+++ www/html/map_script.js
@@ -551,7 +551,7 @@ function removeAllMarkers() {
 		activeInfoLabel.close();
 		activeInfoLabel = null;
 	}
-	markerList = null;
+	markerList = [];
 	activeMarker = null;
 	activeInfoWindow = null;
 	markers = 0;
