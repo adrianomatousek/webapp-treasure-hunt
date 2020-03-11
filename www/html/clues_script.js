8d72579cc5f2bb8b8e8b2520c1a432c224715787
Adriano Matousek <adriano.matousek@gmail.com>
2020-03-11 19:09:03 +0000

    yolo

--- www/html/clues_script.js
+++ www/html/clues_script.js
@@ -8,7 +8,7 @@ function showNextClue(treasureIndex) {
 
         var clueSection = document.getElementById("showClue-" + treasureIndex);
         console.log("Showing next clue " + treasureIndex);
-        newElement = '<div>Clue for treausre with index ' + clues[treasureIndex - 1][activeClue + 1] + '</div>';
+        newElement = '<div>' + clues[treasureIndex - 1][activeClue + 1] + '</div>';
         clueSection.innerHTML += newElement;
 
         activeClue++;
