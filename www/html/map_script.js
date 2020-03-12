ee4348f22aff733e68d14e49da6e7aa9afd555c3
unknown <watcharavuth2000@gmail.com>
2020-03-12 03:46:48 +0000

    icons

--- www/html/map_script.js
+++ www/html/map_script.js
@@ -1087,7 +1087,7 @@ function createDefaultExtraLocations() {
 			// use type id '4' for office or other college locations.
 
 	addExtraMarker(50.735902, -3.538078, 0, 'Student Health Centre', 'Come here when you are feeling sick', 'health.png', '');
-	addExtraMarker(50.7374657, -3.5337152, 4, "David's Office","In Laver you will find the office of David Wakeling.", 'david.png','');
+	addExtraMarker(50.7374657, -3.5337152, 4, "David's Office","In Laver you will find the office of David Wakeling.", 'img/icons/david.png','');
 	//addExtraMarker(90.00, 0.00, 5, "Santa","Merry Christmas", 'santa.png');
 	addExtraMarker(50.737943, -3.537214, 3, "Sports Park", "This is where sports activities take place", 'https://cdn3.iconfinder.com/data/icons/basketball-icons/393/Basketball_Player_With_Ball-512.png','');
 	addExtraMarker(50.735618, -3.533184, 2, "Forum Library", "Our university's library and central hub", 'books.png','');
