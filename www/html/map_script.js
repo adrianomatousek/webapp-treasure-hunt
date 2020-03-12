10976f4ce1c6cc978b5e963111e0db0cab97b01e
unknown <watcharavuth2000@gmail.com>
2020-03-12 03:32:33 +0000

    icons

--- www/html/map_script.js
+++ www/html/map_script.js
@@ -103,12 +103,7 @@ function myMap() {
 
 	//addExtraMarker(50.735902, -3.538078, 0, 'Student Health Centre', '', 'health.png', '');
 
-	//createDefaultExtraLocations();
-	addExtraMarker(50.737943, -3.537214, 3, "Sports Park", "This is where sports activities take place", 'david.png');
-	addExtraMarker(50.735618, -3.533184, 2, "Forum Library", "Our university's library and central hub", 'david.png');
-	addExtraMarker(50.735311, -3.534423, 1, "The Ram Bar", "Most popular cafe in the campus!", 'david.png');
-	addExtraMarker(50.735034, -3.529763, 1, "Lemon Grove", "Bar, Club, Concert, we have it all here!", 'david.png');
-	addExtraMarker(50.734611, -3.529446, 3, "Outdoor Pool", "Wanna go for a swim?", 'david.png');
+	createDefaultExtraLocations();
 }
 
 function setMarkerSize(scaledSize = defaultScaledSize, fontSize, labelOriginHeightOffset = defaultLabelOriginHeightOffset) {
@@ -1093,7 +1088,7 @@ function createDefaultExtraLocations() {
 	addExtraMarker(50.736132, -3.538045, 0, "Student Health Centre");  // <---- maybe add telephone number for contact
 	addExtraMarker(50.7374657, -3.5337152, 4, "David's Office","In Laver you will find the office of David Wakeling.", 'david.png');
 	addExtraMarker(90.00, 0.00, 5, "Santa","Merry Christmas", 'santa.png');
-	addExtraMarker(50.737943, -3.537214, 3, "Sports Park", "This is where sports activities take place", 'sports.png');
+	addExtraMarker(50.737943, -3.537214, 3, "Sports Park", "This is where sports activities take place", 'https://cdn3.iconfinder.com/data/icons/basketball-icons/393/Basketball_Player_With_Ball-512.png');
 	addExtraMarker(50.735618, -3.533184, 2, "Forum Library", "Our university's library and central hub", 'books.png');
 	addExtraMarker(50.735311, -3.534423, 1, "The Ram Bar", "Most popular cafe in the campus!", 'cafe.png');
 	addExtraMarker(50.735034, -3.529763, 1, "Lemon Grove", "Bar, Club, Concert, we have it all here!", 'cafe.png');
