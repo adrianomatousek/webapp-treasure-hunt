6cbb648baddcac53cdb2951f1f0db9181c33280a
janczak <jmateusz26@gmail.com>
2020-03-12 08:38:22 +0000

    testing

--- www/html/TestSuite.js
+++ www/html/TestSuite.js
@@ -400,7 +400,7 @@ function addExtraMarkerTest() {
 	
 	var a1 = assertEquals(extraMarkersList, null);
 	addExtraMarker(50.735402, -3.538078, 0);
-	var a2 = assertEquals(extraMarkersList.length, 1); 
+	var a2 = assertExists(extraMarkersList); 
 	var a3 = assertEquals(extraMarkers, 1);
 	
 	if (a1 && a2 && a3) {
