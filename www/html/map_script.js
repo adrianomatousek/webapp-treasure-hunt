116e3bc7add5d5d5db1a060b42b2ba1c7d915d07
unknown <watcharavuth2000@gmail.com>
2020-03-12 02:45:04 +0000

    icons

--- www/html/map_script.js
+++ www/html/map_script.js
@@ -105,10 +105,10 @@ function myMap() {
 
 	//createDefaultExtraLocations();
 	addExtraMarker(50.737943, -3.537214, 3, "Sports Park", "This is where sports activities take place", 'david.png');
-	addExtraMarker(50.735618, -3.533184, 2, "Forum Library", "Our university's library and central hub", 'books.png');
-	addExtraMarker(50.735311, -3.534423, 1, "The Ram Bar", "Most popular cafe in the campus!", 'cafe.png');
-	addExtraMarker(50.735034, -3.529763, 1, "Lemon Grove", "Bar, Club, Concert, we have it all here!", 'cafe.png');
-	addExtraMarker(50.734611, -3.529446, 3, "Outdoor Pool", "Wanna go for a swim?", 'swim.png');
+	addExtraMarker(50.735618, -3.533184, 2, "Forum Library", "Our university's library and central hub", 'david.png');
+	addExtraMarker(50.735311, -3.534423, 1, "The Ram Bar", "Most popular cafe in the campus!", 'david.png');
+	addExtraMarker(50.735034, -3.529763, 1, "Lemon Grove", "Bar, Club, Concert, we have it all here!", 'david.png');
+	addExtraMarker(50.734611, -3.529446, 3, "Outdoor Pool", "Wanna go for a swim?", 'david.png');
 }
 
 function setMarkerSize(scaledSize = defaultScaledSize, fontSize, labelOriginHeightOffset = defaultLabelOriginHeightOffset) {
