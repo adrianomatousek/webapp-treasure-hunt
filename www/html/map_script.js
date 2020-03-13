e2e7917610e76b52b777de62a342d40ea6d66933
Mateusz Janczak <48460423+janczak@users.noreply.github.com>
2020-03-13 16:18:45 +0000

    icons | typo

--- www/html/map_script.js
+++ www/html/map_script.js
@@ -1176,8 +1176,8 @@ function createDefaultExtraLocations() {
 
 			// use type id '4' for office or other college locations.
 
-	addExtraMarker(50.735902, -3.538078, 0, 'Student Health Centre', 'Come here when you are feeling sick', 'img/icons/health.png', '');
-	addExtraMarker(50.7374657, -3.5337152, 4, "David's Office","In Laver you will find the office of David Wakeling.", 'img/icons/david.png','');
+	addExtraMarker(50.735902, -3.538078, 0, 'Student Health Centre', 'Come here when you are feeling sick', 'img/icons/health.png');
+	addExtraMarker(50.7374657, -3.5337152, 4, "David's Office","In Laver you will find the office of David Wakeling.", 'img/icons/david.png');
 	addExtraMarker(50.737943, -3.537214, 3, "Sports Park", "This is where sports activities take place", 'img/icons/sport-black.png','img/icons/sport-white.png');
 	addExtraMarker(50.735618, -3.533184, 2, "Forum Library", "Our university's library and central hub", 'img/icons/books-black.png','img/icons/books-white.png');
 	addExtraMarker(50.735311, -3.534423, 1, "The Ram Bar", "Most popular cafe in the campus!", 'img/icons/cafe-black.png', 'img/icons/cafe-white.png');
